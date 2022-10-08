ed5783bae7d14f5cfa530da85313019fc9cbb8ca
Romenildo Silva <romenildo2017@outlook.com>
2022-10-07 22:10:47 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -9,22 +9,27 @@ function meuEscopo () {
 
         const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
+        const idade = form.querySelector('.idade');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
         pessoas.push({
             nome: nome.value,
             sobrenome: sobrenome.value,
+            idade: idade.value,
             peso: peso.value,
             altura: altura.value
         });
+    
+
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ` +
+    `${idade.value} ${peso.value} ${altura.value}</p>`
+
     }
-
-    console.log(pessoas)
-
-    resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} ` +
-    `${peso.value} ${altura.value} </p>`;
 
     form.addEventListener('submit', recebeEventoForm);
 }
+
 meuEscopo();
