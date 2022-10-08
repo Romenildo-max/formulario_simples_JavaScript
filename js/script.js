e45369606d18b4d36d69e5f85a345f08ac6bc1ec
Romenildo Silva <romenildo2017@outlook.com>
2022-10-07 21:09:29 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
 
-    form.onsubmit = function (evento) {
+    let contador = 1;
+    function recebeEventoForm (evento) {
         evento.preventDefault();
-        alert(1);
-        console.log('Foi enviado')
-    };
+        console.log(`Form não foi enviado ${contador}`);
+        contador++;
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
 }
 meuEscopo();
